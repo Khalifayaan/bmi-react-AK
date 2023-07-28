86b72e48a2dfa0d9b1d6c0b81a3cf02afd4841d7
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-07-28 19:52:06 +0300

    bmi-react-AK-Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import "./App.css";
 
-function App() {
+
+import React, { useState } from "react";
+
+function BMIApp() {
+  const [name, setName] = useState("");
+  const [height, setHeight] = useState("");
+  const [mass, setMass] = useState("");
+  const [bmi, setBMI] = useState(null);
+
+  function calculateBMI() {
+    if (height && mass) {
+      const bmiValue = (mass / (height / 100) ** 2).toFixed(2);
+      setBMI(bmiValue);
+    }
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="BMIApp">
+      <h2>BMI Calculator</h2>
+      <div className="input-container">
+        <label>Name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="input-container">
+        <label>Height (cm):</label>
+        <input
+          type="number"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
+      </div>
+      <div className="input-container">
+        <label>Mass (kg):</label>
+        <input
+          type="number"
+          value={mass}
+          onChange={(e) => setMass(e.target.value)}
+        />
+      </div>
+      <button onClick={calculateBMI}>Calculate BMI</button>
+      {bmi && (
+        <div className="result">
+          <p>Name: {name}</p>
+          <p>Height: {height} cm</p>
+          <p>Mass: {mass} kg</p>
+          <p>BMI: {bmi}</p>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default BMIApp;
+
